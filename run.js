9b38410d4921c2bb949db76454d939b54af2670f
Kert Ojasoo <kert.ojasoo@rangeforce.com>
2019-10-08 08:19:58 +0000

    Update run.js: added CHANGEME in domain name

--- run.js
+++ run.js
@@ -259,7 +259,7 @@ function start () {
 	app.run({
 		'callback': f,
 		'context': this,
-		'host': 'rubeus.rangeforce.com',
+		'host': 'CHANGEME.rangeforce.com',
 		'port': 443,
 		'protocol': 'wss',
 		// This random string is not cryptographically secure, but it is still
